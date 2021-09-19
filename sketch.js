9cdf618e74bdf943bb5ffd4aeaab3b0b3632ac8e
arjunghosh24 <76735987+arjunghosh24@users.noreply.github.com>
2021-09-19 20:49:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+var hour, hourText;
+
+var bg = "sunrise.png";
+
+function preload() 
+{
+    getTime();
+    
+}
+
+function setup()
+{
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+}
+
+function draw()
+{
+    if(backgroundImg)
+    {
+        background(backgroundImg);
+    }
+
+    Engine.update(engine);
+
+    strokeWeight(4);
+    textSize(20);
+    fill("white");
+    text("Time: " + hourText, width - 1200, 20);
+
+}
+
+async function getTime(){
+
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/kolkata");
+    var responseJSON = await response.json();
+
+    console.log(responseJSON);
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+
+    console.log(hour);
+
+    
+    if(hour>=0 && hour<18 )
+    {
+        bg = "sunrise.png";
+    }
+    else
+    {
+        bg="sunset.png"
+    }
+    backgroundImg = loadImage(bg);
+    hourText = hour;
+}
